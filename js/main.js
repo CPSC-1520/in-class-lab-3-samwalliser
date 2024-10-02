109bc4abc25d6b6c4df32694414b543caccf8e12
samwalliser <146493861+samwalliser@users.noreply.github.com>
2024-10-01 20:15:45 -0600

    completed exercise step 1. linked my javascript file with a console.log, selected the div with the class for javascript-resources and the button with the id of show-resources. Added an event.listener with the click event, also remove the class "d-none". I refreshed the page to look like the outcome for step 1 and it shows correctly matching. Moving onto step 2

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
 // Enter your code below.
+console.log('JavaScript is linked');
+
+// Exercise Step 1: SHow the JavaScript Resources
+
+let resourcesDiv = document.querySelector(".javascript-resources");
+let showResourcesButton = document.getElementById("show-resources");
+
+showResourcesButton.addEventListener("click", (event) => {
+    resourcesDiv.classList.remove("d-none");
+});
+
+// Exercise Step 2:
+
+
+
+
+// Exercise Step 3:
+
+
+
+
+// Exercise Step 4:
